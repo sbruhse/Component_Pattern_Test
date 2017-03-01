3d8ad128630fa4bf1eb174b5ebaf7254f261d452
lape <git_mr@lapete.de>
2017-03-02 00:13:57 +0100

    first commit

---
+++ src/Component.java
@@ -0,0 +1,21 @@
+
+public abstract class Component {
+	
+	protected String name;
+	
+	public void prntCmp()
+	{
+		
+	}
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+}
---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee extends Component {
+
+	public void prntCmp()
+	{
+		System.out.println("  Employee name: " + this.name);
+	}
+	
+	public Employee(String name)
+	{
+		this.name=name;
+	}
+	
+}
